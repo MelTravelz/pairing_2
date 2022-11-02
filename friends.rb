9deb01039804f5ecd3fd55449554318a431458c4
Melony Franchini <meltravelz@gmail.com>
2022-11-02 17:45:38 -0600

    Add another friend

--- friends.rb
+++ friends.rb
@@ -1,7 +1,11 @@
-best_friend = "Patty"
+best_friend = "Susie"
+other_friend = "Patty"
 
 if best_friend == "Lori"
     puts "Hello Lori"
+elsif
+    other_friend == "Patty"
+    puts "Hello Patty"
 else 
     puts "Hello who are you?"
 end
